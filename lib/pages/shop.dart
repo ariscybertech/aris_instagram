b0c428871762e2dfd64220b749995d995b56831f
kali <kali@kali.kali>
2021-12-04 03:05:57 -0500

    Version 1.0.0+6

--- lib/pages/shop.dart
+++ lib/pages/shop.dart
@@ -1,3 +1,4 @@
+import 'package:aris_instagram/util/shop_grid.dart';
 import 'package:flutter/material.dart';
 
 class UserShop extends StatelessWidget {
@@ -6,7 +7,34 @@ class UserShop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('SHOP')),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Container(
+            padding: EdgeInsets.all(8),
+            color: Colors.grey[300],
+            child: Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  color: Colors.grey[500],
+                ),
+                Container(
+                  child: Text(
+                    'Search',
+                    style: TextStyle(
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: ShopGrid(),
     );
   }
 }
---
+++ lib/util/shop_grid.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ShopGrid extends StatelessWidget {
+  final List<String> userPosts = [];
+  ShopGrid({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        itemCount: 20,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.all(2.0),
+            child: Container(color: Colors.deepPurple[100]),
+          );
+        });
+  }
+}
